fc33216bf8132a4ff42f0d4f3b7ae55bbc45597a
Cxlddy <rcds.ciampi@gmail.com>
2026-04-27 15:00:39 -0300

    finalização dos comentários (0.2)

--- src/livros.py
+++ src/livros.py
@@ -1,11 +1,11 @@
-from database.database import dados
+from database.database import dados #importação dos dados da db
 
-class Livros():
+class Livros(): #classe do gerenciamento dos livros
 
-    def __init__(self):
+    def __init__(self): 
        self.livros = dados['livros']
     
-    def adicionar_livro(self, livro):
+    def adicionar_livro(self, livro): #função de adição de um livro, apenas para o adm
 
         novo = {
             'titulo' : livro,
@@ -15,15 +15,15 @@ class Livros():
         self.livros.append(novo)
         print(f'{livro} salvo com sucesso!')
 
-    def remover_livro(self, livro):
+    def remover_livro(self, livro): #mesma coisa, agora, para remoção
 
         self.livros.remove(livro)
 
         print(f'{livro} removido com sucesso!')
     
-    def buscar_livro(self, titulo):
-
-        for livro in self.livros:
+    def buscar_livro(self, titulo): #função para busca do livro, em O(n), olhando um por vez, levando em conta o método de armazenamento
+        #lógica: para cada livro na lista, se o título for igual ao digitado, retorna o livro, caso contrário, retorna None
+        for livro in self.livros: 
             if livro['titulo'] == titulo:
                 return livro
         
--- src/usuarios.py
+++ src/usuarios.py
@@ -35,37 +35,46 @@ class Usuarios(): #classe do gerenciamento dos usuários
             print('Livro já emprestado!')
             return False
 
-        data_obj = datetime.strptime(data, "%Y-%m-%d")
-        prazo = data_obj + timedelta(days=7)
+        data_obj = datetime.strptime(data, "%Y-%m-%d") #conversão da data de string para objeto do datetime para o cálculo
+        prazo = data_obj + timedelta(days=7) #prazo de 7 dias para devolução, calculado com base no dia de hoje
 
-        livro['disponivel'] = False
-
-        usuario['Emprestimos'].append({
-            'data': data_obj,
-            'prazo': prazo,
-            'livro': livro
-        })
-
-        print("Livro emprestado com sucesso!")
-        return True
-
-
-    def emprestar(self, usuario, data, livro):
-
-        if not livro['disponivel']:
-            print('Livro já emprestado!')
-            return False
-
-        data_obj = datetime.strptime(data, "%Y-%m-%d")
-        prazo = data_obj + timedelta(days=7)
-
-        livro['disponivel'] = False
+        livro['disponivel'] = False #marca o livro como indisponivel
 
         usuario['Emprestimos'].append({
             'data': data_obj,
             'prazo': prazo,
             'livro': livro
-        })
+        }) #adiciona o empréstimo a lista do usuário
 
         print("Livro emprestado com sucesso!")
         return True
+
+
+    def devolver(self, usuario, data, livro): #método para devolução 
+
+        hoje = datetime.today() #data de hoje para calcular o atraso, caso haja
+        multa_total = 0
+
+        for emp in usuario['Emprestimos']: #para cada empréstimo do usuário
+            if emp['livro'] == livro: #se o livro for igual ao digitado
+
+                prazo = emp['prazo'] #olha o prazo
+
+                if hoje > prazo: #calcula a multa, sendo 1 real por dia
+                    dias_atraso = (hoje - prazo).days
+                    multa_total = dias_atraso * 1  # R$1 por dia
+
+                usuario['Emprestimos'].remove(emp) #remove o empréstimo da lista
+
+                livro['disponivel'] = True #marca o livro como disponível novamente
+
+                print("Livro devolvido!")
+
+                if multa_total > 0: #mostra a multa, caso haja
+                    print(f"Multa por atraso: R${multa_total}")
+                else:
+                    print("Sem multa!")
+
+                return
+
+        print("Empréstimo não encontrado") #se não for encontrado, mostra mensagem de erro
